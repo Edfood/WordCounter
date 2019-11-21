24c3ff629992a42be79477ffbd5f08638fcb2e32
Edfood <kfed542@gmail.com>
2019-11-22 03:14:57 +0900

    Fix a half-width characters issue

--- word_counter.py
+++ word_counter.py
@@ -1,12 +1,11 @@
 from collections import defaultdict
 from pyknp import Jumanpp
-
 import sys
 
 
 def main():
     if len(sys.argv) != 2:
-        print('need one argument for file name.')
+        print('need one argument for a file.')
         return
 
     file_name = sys.argv[1]
@@ -17,6 +16,10 @@ def main():
         text = fr.readlines()
 
         for line in text:
+            # juman++ doesn't support half-width character
+            line = line.replace(' ', '　')
+            line.translate(str.maketrans(
+                {chr(0x0021 + i): chr(0xFF01 + i) for i in range(94)}))
             analysis = juman.analysis(line.replace('\n', ''))
             for m in analysis.mrph_list():
                 vocab_dict[str(m.midasi)] += 1
